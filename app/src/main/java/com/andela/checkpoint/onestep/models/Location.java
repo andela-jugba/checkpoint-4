6f2a57f596f8ace4382544c827a984b4158e8e56
Joshua Ugba <justjosh001@gmail.com>
2015-11-02 17:34:29 +0100

    Location model and LocationHelper classes

---
+++ app/src/main/java/com/andela/checkpoint/onestep/models/Location.java
@@ -0,0 +1,71 @@
+package com.andela.checkpoint.onestep.models;
+
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * Created by andela-jugba on 11/2/15.
+ */
+public class Location {
+    private String nName;
+    private double nLongitude;
+    private double nLatitude;
+    private Date mDate;
+    private UUID mID;
+    private int mTimesVisited;
+
+    public Location() {
+        mID = UUID.randomUUID();
+        mDate = new Date();
+    }
+
+    public Location(UUID id){
+        mID = id;
+        mDate = new Date();
+    }
+
+    public String getName() {
+        return nName;
+    }
+
+    public void setName(String nName) {
+        this.nName = nName;
+    }
+
+    public double getLongitude() {
+        return nLongitude;
+    }
+
+    public void setLongitude(double nLongitude) {
+        this.nLongitude = nLongitude;
+    }
+
+    public double getLatitude() {
+        return nLatitude;
+    }
+
+    public void setLatitude(double nLatitude) {
+        this.nLatitude = nLatitude;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date mDate) {
+        this.mDate = mDate;
+    }
+
+    public UUID getID() {
+        return mID;
+    }
+
+
+    public int getTimesVisted() {
+        return mTimesVisited;
+    }
+
+    public void setTimesVisited(int mTimesVisited) {
+        this.mTimesVisited = mTimesVisited;
+    }
+}
