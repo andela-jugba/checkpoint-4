d23f7180c784ca6e2ae3c5e65251dbbd323285cc
Joshua Ugba <justjosh001@gmail.com>
2015-11-02 13:58:21 +0100

    Test basic ui elements

--- app/src/test/java/com/andela/checkpoint/onestep/MainActivityTest.java
+++ app/src/test/java/com/andela/checkpoint/onestep/MainActivityTest.java
@@ -1,5 +1,8 @@
 package com.andela.checkpoint.onestep;
 
+import android.widget.Button;
+import android.widget.TextView;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -8,7 +11,9 @@ import org.robolectric.Robolectric;
 import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.annotation.Config;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by andela-jugba on 10/30/15.
@@ -33,6 +38,51 @@ public class MainActivityTest {
     @Test
     public void testMainActivityExits() throws Exception {
         assertNotNull(testMainActivity);
+
+    }
+
+    @Test
+    public void testBasicUiElements() throws Exception {
+
+        //given that I am on the main screen of the app
+        thenIShouldSeeTheBasicUiElementsInPosition();
+        //when I click on Start,
+        thenTheSetStepButtonShouldNotBeEnabled();
+        thenTheTextShouldChangeToStop();
+
+
+    }
+
+    private void thenTheTextShouldChangeToStop() {
+        TextView testStartView = (TextView) testMainActivity.findViewById(R.id.textViewPlay);
+        assertEquals("The text should be equal", testStartView.getText(), "stop");
+    }
+
+    private void thenTheSetStepButtonShouldNotBeEnabled() {
+        TextView testStartView = (TextView) testMainActivity.findViewById(R.id.textViewPlay);
+        testStartView.performClick();
+        Button testSetButton = (Button) testMainActivity.findViewById(R.id.buttonSetStep);
+        assertTrue(!testSetButton.isEnabled());
+
+
+    }
+
+    private void thenIShouldSeeTheBasicUiElementsInPosition() {
+        Button testSetButton = (Button) testMainActivity.findViewById(R.id.buttonSetStep);
+        assertNotNull("This should be there when the app loads", testSetButton);
+        assertEquals("Text of the button", testSetButton.getText(), "SET STEP");
+
+        Button testLogButton = (Button) testMainActivity.findViewById(R.id.buttonLog);
+        assertNotNull("This should be there when the app loads up", testLogButton);
+        assertEquals("The Text of the button should be", testLogButton.getText(), "LOGS");
+
+        TextView testStartView = (TextView) testMainActivity.findViewById(R.id.textViewPlay);
+        assertNotNull("This be loaded up", testStartView);
+        assertEquals("The text should be equal", testStartView.getText(), "start");
+
+        TextView testSecView = (TextView) testMainActivity.findViewById(R.id.textViewSec);
+        assertNotNull("This view should be loaded", testSecView);
+        assertEquals("The text should be equal", testSecView.getText(), "secs");
 
     }
 }
